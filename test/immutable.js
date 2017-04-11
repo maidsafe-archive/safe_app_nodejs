d392ecb1509eb48b1589df25d8697ceffe8a5c55
bochaco <gabrielviganotti@gmail.com>
2017-04-11 11:25:19 -0300

    test/Adding ImmutableData test

--- test/immutable.js
+++ test/immutable.js
@@ -3,6 +3,9 @@ const h = require('./helpers');
 
 describe('Immutable Data', () => {
   const app = h.createAuthenticatedTestApp();
+  const TAG_TYPE = 15639;
+  const TEST_NAME_PUBLIC = 'test-name-public--01010101010101';
+
   it('write read simple ', () => {
     const testString = `test-${Math.random()}`;
 
@@ -14,5 +17,27 @@ describe('Immutable Data', () => {
            .then((res) => {
              should(res.toString()).equal(testString);
            })));
+  });
+
+  it.only('store address in a MD', () => {
+    const testString = `test-${Math.random()}`;
+
+    return app.immutableData.create()
+      .then((w) => w.write(testString)
+        .then(() => w.close())
+        .then((addr) => app.mutableData.newPublic(TEST_NAME_PUBLIC, TAG_TYPE)
+          .then((md) => md.quickSetup({'key1': addr}))
+        ))
+      .then(() => app.mutableData.newPublic(TEST_NAME_PUBLIC, TAG_TYPE))
+      .then((md) => md.getEntries())
+      .then((entries) => entries.forEach((key, value) => {
+          app.immutableData.fetch(value)
+          .then((r) => r.read())
+          .then((res) => {
+            console.log("Test string: ", res.toString());
+            should(res.toString()).equal(testString);
+          });      
+        })
+      )
   });
 });
