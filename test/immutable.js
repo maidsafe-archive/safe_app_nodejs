b2dd61e6abe35cdfa009b2a051d9025110092d81
bochaco <gabrielviganotti@gmail.com>
2017-04-11 12:23:48 -0300

    test/Adding test to fetch immutableData stored in MD

--- test/immutable.js
+++ test/immutable.js
@@ -19,7 +19,7 @@ describe('Immutable Data', () => {
            })));
   });
 
-  it.only('store address in a MD', () => {
+  it.skip('store address in a MD', () => {
     const testString = `test-${Math.random()}`;
 
     return app.immutableData.create()
@@ -29,14 +29,39 @@ describe('Immutable Data', () => {
           .then((md) => md.quickSetup({'key1': addr}))
         ))
       .then(() => app.mutableData.newPublic(TEST_NAME_PUBLIC, TAG_TYPE))
+      .then((md) => md.get('key1'))
+      .then((value) => app.immutableData.fetch(value.buf))
+      .then((r) => r.read())
+      .then((res) => {
+        console.log("Test string: ", res.toString());
+        should(res.toString()).equal(testString);
+      })
+  });
+
+  it.skip('store address in a serialised/deserialised  MD', () => {
+    const testString = `test-${Math.random()}`;
+
+    return app.immutableData.create()
+      .then((w) => w.write(testString)
+        .then(() => w.close())
+        .then((addr) => app.mutableData.newPublic(TEST_NAME_PUBLIC, TAG_TYPE)
+          .then((md) => md.serialise())
+          .then((serial) => app.mutableData.fromSerial(serial))
+            .then((md) => md.getEntries())
+            .then((entries) => entries.mutate())
+            .then((mut) => mut.insert('key1', addr)
+              .then(() => md.applyEntriesMutation(mut))
+            )
+        ))
+      .then(() => app.mutableData.newPublic(TEST_NAME_PUBLIC, TAG_TYPE))
       .then((md) => md.getEntries())
       .then((entries) => entries.forEach((key, value) => {
-          app.immutableData.fetch(value)
+          app.immutableData.fetch(value.buf)
           .then((r) => r.read())
           .then((res) => {
             console.log("Test string: ", res.toString());
             should(res.toString()).equal(testString);
-          });      
+          });
         })
       )
   });
