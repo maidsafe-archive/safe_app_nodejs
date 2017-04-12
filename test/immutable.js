a0b13674575ce4cf2f260b636ae256d3ca876652
bochaco <gabrielviganotti@gmail.com>
2017-04-12 17:42:55 -0300

    test/Re-enabling tests and minor fixes to some of other tests

--- test/immutable.js
+++ test/immutable.js
@@ -4,7 +4,6 @@ const h = require('./helpers');
 describe('Immutable Data', () => {
   const app = h.createAuthenticatedTestApp();
   const TAG_TYPE = 15639;
-  const TEST_NAME_PUBLIC = 'test-name-public--01010101010101';
 
   it('write read simple ', () => {
     const testString = `test-${Math.random()}`;
@@ -19,16 +18,17 @@ describe('Immutable Data', () => {
            })));
   });
 
-  it.skip('store address in a MD', () => {
+  it('store address in a MD', () => {
     const testString = `test-${Math.random()}`;
+    const testXorName = h.createRandomXorName();
 
     return app.immutableData.create()
       .then((w) => w.write(testString)
         .then(() => w.close())
-        .then((addr) => app.mutableData.newPublic(TEST_NAME_PUBLIC, TAG_TYPE)
+        .then((addr) => app.mutableData.newPublic(testXorName, TAG_TYPE)
           .then((md) => md.quickSetup({ key1: addr }))
         ))
-      .then(() => app.mutableData.newPublic(TEST_NAME_PUBLIC, TAG_TYPE))
+      .then(() => app.mutableData.newPublic(testXorName, TAG_TYPE))
       .then((md) => md.get('key1'))
       .then((value) => app.immutableData.fetch(value.buf))
       .then((r) => r.read())
@@ -37,22 +37,25 @@ describe('Immutable Data', () => {
       });
   });
 
-  it.skip('store address in a serialised/deserialised  MD', () => {
+  it('store address in a serialised/deserialised  MD', () => {
     const testString = `test-${Math.random()}`;
+    const testXorName = h.createRandomXorName();
 
     return app.immutableData.create()
       .then((w) => w.write(testString)
         .then(() => w.close())
-        .then((addr) => app.mutableData.newPublic(TEST_NAME_PUBLIC, TAG_TYPE)
+        .then((addr) => app.mutableData.newPublic(testXorName, TAG_TYPE)
+          .then((md) => md.quickSetup())
           .then((md) => md.serialise())
           .then((serial) => app.mutableData.fromSerial(serial))
-            .then((md) => md.getEntries()
+          .then((md) => md.getEntries()
             .then((entries) => entries.mutate())
             .then((mut) => mut.insert('key1', addr)
               .then(() => md.applyEntriesMutation(mut))
             ))
-        ))
-      .then(() => app.mutableData.newPublic(TEST_NAME_PUBLIC, TAG_TYPE))
+          )
+        )
+      .then(() => app.mutableData.newPublic(testXorName, TAG_TYPE))
       .then((md) => md.getEntries())
       .then((entries) => entries.forEach((key, value) => {
         app.immutableData.fetch(value.buf)
