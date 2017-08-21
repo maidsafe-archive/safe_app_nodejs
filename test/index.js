8b0fd2fe04f2acf5d40d025b381a8994279afcd5
hunterlester <guilfordhunter@gmail.com>
2017-08-21 09:05:42 -0700

    CI fix

--- test/index.js
+++ test/index.js
@@ -17,7 +17,7 @@ describe('Smoke testing', () => {
 
 describe('External API', () => {
   describe('fromAuthURI', () => {
-<<<<<<< HEAD
+
     it('should authenticate', () => {
       const uri = 'safe-bmV0Lm1haWRzYWZlLnRlc3Qud2ViYXBwLmlk:AQAAAGSv7oQAAAAAAAAAACAAAAAAAAAAGQ1zYg9iFKof2TVkAPp0R2kjU9DDWmmR_uAXBYvaeIAgAAAAAAAAAKecZc5pOSeoU53v43RdoTscGQbuAO0hF6HA_4ou9GJnIAAAAAAAAADsycX-1RCaNJxnYf6ka1pLncSez4w4PmPIS5lau_IblkAAAAAAAAAAbZdkFJ6Ydhh_OwA7mfYcnta_95k2xRazJsDSeMFGj3vsycX-1RCaNJxnYf6ka1pLncSez4w4PmPIS5lau_IbliAAAAAAAAAAx559E774w-6AWnIXBSm0NWOBW2zr8TOPThmdIeEsoFEgAAAAAAAAAHRNdser-WDOLIBGsDfRbNI304vnYILXI1JZC96tiFvzAAAAAAAAAAAAAAAAAAAAAG7Di2O1ssjN0izb88iclOKj7WD5LtaVriMIrLBbVRHimDoAAAAAAAAYAAAAAAAAAH2p2f2I4yuQPLkSJE_u9-PtM1WD7E65ZA==';
       const appInfo = {
@@ -26,17 +26,15 @@ describe('External API', () => {
         vendor: 'MaidSafe Ltd.',
         scope: null
       };
-=======
+
     it('should connect registered', () => fromAuthURI(appInfo, h.authUris.registeredUri, null, { log: false })
         .then((app) => should(app.auth.registered).be.true())
     );
->>>>>>> 822c355d0046db58435eb1e715ff08bb72ca1388
 
     it('should connect unregistered', () => fromAuthURI(appInfo, h.authUris.unregisteredUri, null, { log: false })
         .then((app) => should(app.auth.registered).not.be.true())
     );
 
-<<<<<<< HEAD
     it('should connect unregistered', () => {
       const uri = 'safe-dW5yZWdpc3RlcmVk:AQAAAMga9SYCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=';
       const appInfo = {
@@ -45,11 +43,10 @@ describe('External API', () => {
         vendor: 'MaidSafe Ltd.',
         scope: null
       };
-=======
+
     it('should connect with authorised containers', () => fromAuthURI(appInfo, h.authUris.containersUri, null, { log: false })
         .then((app) => should(app.auth.registered).be.true())
     );
->>>>>>> 822c355d0046db58435eb1e715ff08bb72ca1388
 
     it('should connect with authorised shared MD', () => fromAuthURI(appInfo, h.authUris.sharedMdataUri, null, { log: false })
         .then((app) => should(app.auth.registered).be.true())
