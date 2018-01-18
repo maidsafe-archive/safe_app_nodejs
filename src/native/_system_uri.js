ae9994b513ae53ffd98b34eb95d506469b0d07b7
Gabriel Viganotti <bochaco@users.noreply.github.com>
2018-01-18 16:19:26 -0300

    fix/Pass the system URI's execution path as an array of arguments to support white spaces in it (#194)
    * feat/Send system URI's exec command as an array of arguments to system_uri lib
    * docs/Updating CHANGELOG to have an Unreleased section
    * chore/Adapt to changes in system_uri lib API which now returns results as a pointer to FfiResult struct
    * chore/system_uri: Upgrade system_uri lib to v0.4.0
    * test/system_uri: Add tests for customExecPath param of the appInfo

--- src/native/_system_uri.js
+++ src/native/_system_uri.js
@@ -5,6 +5,9 @@ const { SYSTEM_URI_LIB_FILENAME } = require('../consts');
 const makeFfiError = require('./_error.js');
 const h = require('./helpers');
 const t = require('./types');
+const ArrayType = require('ref-array');
+
+const StringArray = ArrayType(ref.types.CString);
 
 const dir = path.dirname(__filename);
 let ffi = null;
@@ -16,7 +19,8 @@ const init = (options) => {
     install: ["void", ['string', //bundle
       'string', //vendor
       'string', //name
-      'string', //exec
+      StringArray, //exec args
+      t.usize, //exec args length
       'string', //icon
       'string', //schemes,
       'pointer', // userdata
@@ -26,10 +30,11 @@ const init = (options) => {
 };
 
 const _handleError = (resolve,  reject) => {
-  return FFI.Callback("void", [t.VoidPtr, t.FfiResult],
-    (userData, result) => {
+  return FFI.Callback("void", [t.VoidPtr, t.FfiResultPtr],
+    (userData, resultPtr) => {
+      const result = h.makeFfiResult(resultPtr);
       if (result.error_code !== 0) {
-        return reject(makeFfiError(err.error_code, err.error_description));
+        return reject(makeFfiError(result.error_code, result.error_description));
       }
       return resolve();
     }
@@ -52,19 +57,22 @@ const openUri = (uri) => {
 
 
 const registerUriScheme = (appInfo, schemes) => {
+  if (!ffi) {
+    return;
+  }
+  if (appInfo.exec && !Array.isArray(appInfo.exec)) {
+    throw new Error("Exec command must be an array of string arguments");
+  }
   const bundle = appInfo.bundle || appInfo.id;
-  const exec = appInfo.exec ? appInfo.exec : process.execPath;
+  const exec = appInfo.exec ? new StringArray(appInfo.exec) : new StringArray([process.execPath]);
   const vendor = appInfo.vendor.replace(/\s/g, '-');
   const name = appInfo.name.replace(/\s/g, '-');
   const icon = appInfo.icon;
   const joinedSchemes = schemes.join ? schemes.join(',') : schemes;
-  if (!ffi) {
-    return;
-  }
   return new Promise((resolve, reject) => {
     try {
       const cb = _handleError(resolve, reject);
-      ffi.install(bundle, vendor, name, exec, icon, joinedSchemes, ref.NULL, cb);
+      ffi.install(bundle, vendor, name, exec, exec.length, icon, joinedSchemes, ref.NULL, cb);
     } catch (err) {
       return reject(err);
     }
